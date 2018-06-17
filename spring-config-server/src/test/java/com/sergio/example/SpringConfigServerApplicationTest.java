589646c87fb5fc8558cb8cfd4327a2dbb719d1b8
sergio <sverde@autentia.com>
2018-06-17 14:45:56 +0200

    spring config server

---
+++ spring-config-server/src/test/java/com/sergio/example/SpringConfigServerApplicationTest.java
@@ -0,0 +1,16 @@
+package com.sergio.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringConfigServerApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
